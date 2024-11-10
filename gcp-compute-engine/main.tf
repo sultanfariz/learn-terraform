a8d2d69b80fcd15ed2a808fb42786de798393596
sultanfariz <hydeblazack@gmail.com>
2024-11-10 23:41:20 +0700

    feat: add firewalls to allow http and https

--- gcp-compute-engine/main.tf
+++ gcp-compute-engine/main.tf
@@ -1,5 +1,14 @@
 # main.tf
 
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.10.0"
+    }
+  }
+}
+
 provider "google" {
   project = "bunder-432813"
   region  = "us-central1"
@@ -8,13 +17,6 @@ provider "google" {
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
-}
-
-resource "google_compute_subnetwork" "subnet" {
-  name          = "terraform-subnet"
-  ip_cidr_range = "10.0.0.0/24"
-  region        = "us-central1"
-  network       = google_compute_network.vpc_network.name
 }
 
 resource "google_compute_firewall" "allow_internal" {
@@ -35,7 +37,7 @@ resource "google_compute_firewall" "allow_internal" {
     ports    = ["0-65535"]
   }
 
-  source_ranges = ["10.0.0.0/24"]
+  source_ranges = ["0.0.0.0/0"]
 }
 
 resource "google_compute_firewall" "allow_ssh" {
@@ -50,6 +52,32 @@ resource "google_compute_firewall" "allow_ssh" {
   source_ranges = ["0.0.0.0/0"]
 }
 
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+# vm instances
 resource "google_compute_instance" "vm_instance" {
   name         = "test-vm"
   machine_type = "f1-micro"
